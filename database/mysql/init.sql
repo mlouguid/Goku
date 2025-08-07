8530730bfccf31988c1d2a7c9e234a27fa594b9e
Marouane <82709225+mlouguid@users.noreply.github.com>
2025-08-07 22:17:17 +0100

    Super Guko  > full transformation infra docker

---
+++ database/mysql/init.sql
@@ -0,0 +1,13 @@
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles WHERE rolname = 'db_beta'
+   ) THEN
+      CREATE ROLE db_beta LOGIN PASSWORD 'beta@secure@pass';
+   END IF;
+END
+$$;
+
+-- Grant privileges on the DB
+GRANT ALL PRIVILEGES ON DATABASE db_beta TO db_beta;
